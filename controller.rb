b406dcc7aba8614d0b19702e2352652438b471fc
Rachael Miller & Terence Stone <pair+raemilla.terencestone@devbootcamp.com>
2016-08-12 16:45:57 -0400

    Revise controller

--- controller.rb
+++ controller.rb
@@ -10,17 +10,21 @@ class Controller
     @deck.populate_deck(@parser.card_arr)
   end
 
+  def full_deck
+    @full_deck = @deck.full_deck
+  end
+
   def total_questions
-    @total_questions = @deck.full_deck.length
+    @total_questions = @full_deck.length
   end
 
   def run
     create_deck
-    create_deck.each do |card|
-    @view.#display question(card)
-    @view.#get user input
-    @view.#say if it's right
-    @view.#
+    @full_deck.each do |card|
+      @view.#display question(card)
+      @view.#get user input
+      @view.#say if it's right
+      @view.#
     end
     @view.#display number of questions correct out of total
   end
